2c641851f78feddc9b1dfd7a8494fd5a0a4613ee
Faf-dev <noaillesjulien@gmail.com>
2025-08-17 23:34:01 +0200

    modification de la route du backend railway

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://votre-backend.railway.app/api' // Backend Railway
+  apiUrl: 'https://votre-backend.up.railway.app' // Backend Railway
 };
